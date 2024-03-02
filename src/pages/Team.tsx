56455eac5dc3a111a455cb3d2cd04c8e1dc6132c
Jonathan Bernard Widjajakusuma <jbw7@illinois.edu>
2024-03-01 20:09:49 -0600

    officers page draft

--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -1,11 +1,31 @@
 import React from "react";
+import OfficerCards from "../components/OfficerCards";
 
 const TeamPage: React.FC = () => {
   return (
     <div>
-      TEAMPAGE
+      <div className="w-full h-[600px] flex justify-center items-end">
+        <img
+          src={process.env.PUBLIC_URL + "/officers/background.JPG"}
+          alt="Background"
+          className="object-cover w-full h-full"
+        />
+      </div>
+      <div className="flex justify-center my-[100px]">
+        <h1 className="text-6xl">MEET OUR OFFICERS</h1>
+      </div>
+      <div className="h-[1000px] w-full flex justify-center mt-[50px]">
+        <div className="w-[80%]">
+          <div className="grid grid-cols-3 gap-y-14 justify-items-center">
+            <OfficerCards />
+            <OfficerCards />
+            <OfficerCards />
+            <OfficerCards />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TeamPage
+export default TeamPage;
