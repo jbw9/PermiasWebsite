2b18efc99d05f294e34adbd4ae4c1a12a69b5d39
Jonathan Bernard Widjajakusuma <jbw7@illinois.edu>
2024-03-02 02:45:21 -0600

    adjusted dimensions

--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -15,7 +15,7 @@ const TeamPage: React.FC = () => {
       <div className="flex justify-center my-[75px]">
         <h1 className="text-6xl font-semibold">Meet Our Officers</h1>
       </div>
-      <div className="h-screen w-full flex justify-center mt-[50px]">
+      <div className="h-[3000px] w-full flex justify-center mt-[50px]">
         <div className="w-[80%]">
           <div className="grid grid-cols-3 gap-y-14 justify-items-center">
             {teamMembers.map((member, index) => (
