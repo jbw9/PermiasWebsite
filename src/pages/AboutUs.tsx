a01730d2e30979bc399cee4bf12662aa38f61792
wrjodjana <141975732+wrjodjana@users.noreply.github.com>
2024-03-02 16:36:24 -0600

    edited About Us page

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -3,23 +3,18 @@ import React from "react";
 const AboutUsPage: React.FC = () => {
   return (
     <div className="">
-      <div className="mb-12 ml-40 mt-[40px]">
-        <span className=" text-4xl">About Us</span>
-
-        <div className="mt-4 flex">
-          <span className="pr-[50px]">
-            PERMIAS UIUC or commonly known as the Indonesian Students Club (ISC)
-            is an Indonesian community in the heart of Urbana - Champaign. We
-            strive to unite the Indonesian community within the University of
-            Illinois at Urbana-Champaign while also promoting our culture to the
-            greater community in Illinois.
-          </span>
-          <img
-            className="mr-[200px] pl-[100px]"
-            src={process.env.PUBLIC_URL + "/AboutUs/image1.JPG"}
-            alt="PERMIAS LOGO"
-            width={600}
-          />
+      <div className="container mx-auto px-4">
+        <h1 className="text-2xl font-bold mb-4">About Us</h1>
+        <div className="flex flex-col md:flex-row">
+          <div className="md:w-1/2">
+            <span className="text-lg mb-4 md:mb-0">
+              PERMIAS UIUC or commonly known as the Indonesian Students Club (ISC) is an Indonesian community in the heart of Urbana - Champaign. We strive to unite the Indonesian community within the University of Illinois at
+              Urbana-Champaign while also promoting our culture to the greater community in Illinois.
+            </span>
+          </div>
+          <div className="md:w-1/2 flex justify-center md:justify-start">
+            <img className="max-w-xs md:max-w-sm lg:max-w-md" src={process.env.PUBLIC_URL + "/AboutUs/image1.JPG"} alt="PERMIAS LOGO" width={600} />
+          </div>
         </div>
       </div>
     </div>
