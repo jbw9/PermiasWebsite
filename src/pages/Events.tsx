f5e9ee4dc5c545de926a5f95dcd48413870d1a71
Jonathan Bernard Widjajakusuma <jbw7@illinois.edu>
2024-03-03 04:43:23 -0600

    events page draft

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const EventPage: React.FC = () => {
-  return <div>HOMEPAGE</div>;
+  return (
+    <div>
+      <div className="w-full h-[600px] flex justify-center items-end">
+        <img
+          src={process.env.PUBLIC_URL + "/events/main.jpg"}
+          alt="Background"
+          className="object-cover w-full h-full"
+        />
+      </div>
+      <div className="flex flex-col">
+        <span className="text-5xl font-semibold">Upcoming Events</span>
+        <div className="w-full h-[400px] bg-slate-500"></div>
+        <span className="text-5xl font-semibold mt-[40px]">Past Events</span>
+        <div className="w-full h-[400px] bg-slate-500"></div>
+      </div>
+    </div>
+  );
 };
 
 export default EventPage;
