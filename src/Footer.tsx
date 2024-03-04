899ba0a598f18fc2cca5fc12eda5d9db9c28433c
Jonathan Bernard Widjajakusuma <jbw7@illinois.edu>
2024-03-04 02:46:50 -0600

    homepage content draft

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -14,44 +14,6 @@ const Footer: React.FC = () => {
           permias.uiuc@gmail.com
         </a>
       </div>
-      <div className="flex">
-        <a
-          href="https://web.whatsapp.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src={process.env.PUBLIC_URL + "/footer/whatsapp.svg"}
-            alt="Whatsapp Logo"
-            width={50}
-            className="mx-10"
-          />
-        </a>
-        <a
-          href="https://www.instagram.com/permiasuiuc/?hl=en"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src={process.env.PUBLIC_URL + "/footer/instagram.svg"}
-            alt="Instagram Logo"
-            width={50}
-            className="mx-10"
-          />
-        </a>
-        <a
-          href="https://www.facebook.com/checkpoint/1501092823525282/?next=https%3A%2F%2Fwww.facebook.com%2Fisc.uiuc%2F"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src={process.env.PUBLIC_URL + "/footer/facebook.svg"}
-            alt="Facebook Logo"
-            width={50}
-            className="mx-10"
-          />
-        </a>
-      </div>
       <div className="items-end m-7">
         <div className="text-xl">New Student?</div>
         <div>
