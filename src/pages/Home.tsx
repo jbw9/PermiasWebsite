770b69b95558c00f695e5c0bdcb86aab2739ade4
hindra2 <harold.indra@gmail.com>
2024-03-04 02:02:04 -0600

    fixed video url

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const HomePage: React.FC = () => {
-  return <div></div>;
-};
+  return (
+    <div>
+      <div className="">
+        <BackgroundVideo source={process.env.PUBLIC_URL + "/Home/backgroundvid.mp4"} children={<WebsiteTitle />}/>
+      </div>
+      <div className="h-[1000px]">
+        Placeholder
+      </div>
+    </div>
+)};
 
 export default HomePage;
