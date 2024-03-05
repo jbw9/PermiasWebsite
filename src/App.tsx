550245d2ceeb26376a5c65eda4f0d9c0f8ac3579
Jonathan Bernard Widjajakusuma <jbw7@illinois.edu>
2024-03-05 15:58:51 -0600

    modified routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,38 @@
-import React from 'react';
+import React from "react";
 
 // Components
 import Header from "./Header";
 import Footer from "./Footer";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
+// Pages
+import HomePage from "./pages/Home";
+import TeamPage from "./pages/Team";
+import AboutUsPage from "./pages/AboutUs";
+import EventPage from "./pages/Events";
+import GuidePage from "./pages/Guide";
+import MerchPage from "./pages/Merch";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div className="flex flex-col h-screen justify-between">
-      <Header />
-      <Footer />
-    </div>
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/team" element={<TeamPage />} />
+            <Route path="/about-us" element={<AboutUsPage />} />
+            <Route path="/events" element={<EventPage />} />
+            <Route path="/guide" element={<GuidePage />} />
+            <Route path="/merch" element={<MerchPage />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,61 +1,37 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
-
-// Pages
-import HomePage from "./pages/Home";
-import TeamPage from "./pages/Team";
-import AboutUsPage from "./pages/AboutUs";
-import EventPage from "./pages/Events";
-import GuidePage from "./pages/Guide";
-import MerchPage from "./pages/Merch";
-import NotFound from "./pages/NotFound";
+import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
   return (
-    <Router>
-      <div className="flex items-center justify-between w-full p-2 mx-auto bg-">
-        <Link to="/">
-          <img
-            src={process.env.PUBLIC_URL + "/mainLogo.png"}
-            alt="PERMIAS LOGO"
-            width={250}
-          />
+    <div className="flex items-center justify-between w-full p-2">
+      <Link to="/">
+        <img
+          src={process.env.PUBLIC_URL + "/mainLogo.png"}
+          alt="PERMIAS LOGO"
+          width={250}
+        />
+      </Link>
+      <nav className="flex items-center ml-4">
+        <Link to="/" className="mx-2 text-2xl font-semibold">
+          Home
         </Link>
-        <div className="">
-          <nav className="flex items-center ml-4">
-            <Link to="/">
-              <div className="mx-2 text-2xl font-semibold">Home</div>
-            </Link>
-            <Link to="/about-us">
-              <div className="mx-2 text-2xl font-semibold">About Us</div>
-            </Link>
-            <Link to="/team">
-              <div className="mx-2 text-2xl font-semibold">Team</div>
-            </Link>
-            <Link to="/events">
-              <div className="mx-2 text-2xl font-semibold">Events</div>
-            </Link>
-            <Link to="/merch">
-              <div className="mx-2 text-2xl font-semibold">Merch</div>
-            </Link>
-            <Link to="/guide">
-              <div className="mx-2 text-2xl font-semibold">Guide</div>
-            </Link>
-          </nav>
-        </div>
-      </div>
-      <div>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/team" element={<TeamPage />} />
-          <Route path="/about-us" element={<AboutUsPage />} />
-          <Route path="/events" element={<EventPage />} />
-          <Route path="/guide" element={<GuidePage />} />
-          <Route path="/merch" element={<MerchPage />} />
-          <Route path="/*" element={<NotFound />} />
-        </Routes>
-      </div>
-    </Router>
+        <Link to="/about-us" className="mx-2 text-2xl font-semibold">
+          About Us
+        </Link>
+        <Link to="/team" className="mx-2 text-2xl font-semibold">
+          Team
+        </Link>
+        <Link to="/events" className="mx-2 text-2xl font-semibold">
+          Events
+        </Link>
+        <Link to="/merch" className="mx-2 text-2xl font-semibold">
+          Merch
+        </Link>
+        <Link to="/guide" className="mx-2 text-2xl font-semibold">
+          Guide
+        </Link>
+      </nav>
+    </div>
   );
 };
 
