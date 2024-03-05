024acf2eff2b2527545b93423f3d9cfeeba34f30
Jonathan Bernard Widjajakusuma <jbw7@illinois.edu>
2024-03-05 01:39:38 -0600

    initial commit

---
+++ src/pages/Guide.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GuidePage: React.FC = () => {
+  return (
+    <div>
+      <div>guide page</div>
+    </div>
+  );
+};
+
+export default GuidePage;
---
+++ src/pages/Support.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SupportPage: React.FC = () => {
+  return (
+    <div>
+      <div>support page</div>
+    </div>
+  );
+};
+
+export default SupportPage;
