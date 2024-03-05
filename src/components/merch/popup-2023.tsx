2ad11851dc9be4a9922c3471abd63c3604d34e12
Jonathan Bernard Widjajakusuma <jbw7@illinois.edu>
2024-03-05 03:17:52 -0600

    completed merch popup

---
+++ src/components/merch/popup-2023.tsx
@@ -0,0 +1,112 @@
+import React, { useRef, useEffect, useState } from "react";
+
+interface PropOpen {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const Popup2023: React.FC<PropOpen> = ({ isOpen, onClose }) => {
+  const availableSizes = ["XXS", "S", "M", "XL", "XXL"];
+  const popupRef = useRef<HTMLDivElement>(null);
+  const [visible, setVisible] = useState(false);
+  const [animate, setAnimate] = useState(false);
+
+  useEffect(() => {
+    if (isOpen) {
+      setVisible(true);
+      setTimeout(() => setAnimate(true), 10);
+    } else {
+      setAnimate(false);
+      setTimeout(() => setVisible(false), 500);
+    }
+  }, [isOpen]);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        popupRef.current &&
+        !popupRef.current.contains(event.target as Node)
+      ) {
+        onClose();
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [onClose]);
+
+  if (!visible) {
+    return null;
+  }
+
+  return (
+    <div
+      className={`fixed inset-0 flex items-center justify-center bg-gray-600 bg-opacity-60 ${
+        animate ? "opacity-100" : "opacity-0"
+      } transition-opacity duration-500`}
+      onClick={onClose}
+    >
+      <div
+        ref={popupRef}
+        onClick={(e) => e.stopPropagation()}
+        className={`relative h-[600px] w-[1200px] bg-white rounded-2xl overflow-hidden ${
+          animate ? "scale-100" : "scale-95"
+        } transition-transform duration-500 ease-out flex`}
+      >
+        <div>
+          <img
+            className="mt-[30px] ml-[15px]"
+            src={process.env.PUBLIC_URL + "/merch/2021_Hoodie_Full.png"}
+            alt="2021 hoodie"
+            width={825}
+          />
+        </div>
+        <div className="flex flex-col justify-between ml-[10px]">
+          <div>
+            <span className="block mt-[20px] text-3xl mb-[-10px]">
+              2021 Permias Hoodie
+            </span>
+            <span className="block mt-[20px] text-2xl">$25</span>
+            <span className="block mt-[20px] text-lg mb-[20px]">
+              Material: .....
+            </span>
+            <span className="block text-lg">Sizes:</span>
+            <div className="grid grid-cols-4 gap-4 p-4">
+              {["XXS", "XS", "S", "M", "L", "XL", "XXL", "XXXL"].map((size) => (
+                <div
+                  key={size}
+                  className={`relative flex items-center justify-center w-15 h-10 border-2 rounded-full cursor-pointer ${
+                    availableSizes.includes(size)
+                      ? "hover:black hover:bg-footer hover:text-white"
+                      : "text-gray-500 cursor-not-allowed"
+                  }`}
+                >
+                  {size}
+                  {!availableSizes.includes(size) && (
+                    <div className="absolute flex items-center justify-center w-full h-full">
+                      <div className="w-[2px] h-full bg-black transform -rotate-45"></div>
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="flex justify-center pb-4">
+            <a
+              href="https://www.google.com/forms/about/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="text-3xl text-white bg-footer rounded-3xl h-[45px] w-[250px] mb-[50px]">
+                Buy
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Popup2023;
--- src/pages/Merch.tsx
+++ src/pages/Merch.tsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import Popup2023 from "../components/merch/popup-2023";
 
 const MerchPage: React.FC = () => {
+  const [isPopupOpen, setPopupOpen] = useState(false);
+
+  const openPopup = () => {
+    setPopupOpen(true);
+  };
+
+  const closePopup = () => {
+    setPopupOpen(false);
+  };
   return (
     <div>
       <div className="w-full h-[600px] flex justify-center items-end">
@@ -12,15 +22,18 @@ const MerchPage: React.FC = () => {
       </div>
       <div className="h-[1200px] w-full mt-[100px] flex flex-col items-center px-[100px]">
         <div className="flex">
-          <div className="h-[660px] w-[450px] mr-[200px] bg-merchbg rounded-2xl shadow-2xl flex flex-col justify-center items-center">
-            <img
-              src={process.env.PUBLIC_URL + "/merch/2021_Hoodie_Main.png"}
-              alt="2021 hoodie"
-              width={450}
-            />
-            <span className="text-4xl mt-[25px]">2021 Permias Hoodie</span>
-            <span className="text-2xl">$25</span>
-          </div>
+          <button onClick={openPopup}>
+            <div className="h-[660px] w-[450px] mr-[200px] bg-merchbg rounded-2xl shadow-2xl flex flex-col justify-center items-center">
+              <img
+                src={process.env.PUBLIC_URL + "/merch/2021_Hoodie_Main.png"}
+                alt="2021 hoodie"
+                width={450}
+              />
+              <span className="text-4xl mt-[25px]">2021 Permias Hoodie</span>
+              <span className="text-2xl">$25</span>
+            </div>
+          </button>
+          <Popup2023 isOpen={isPopupOpen} onClose={closePopup} />
           <div className="h-[660px] w-[450px] bg-merchbg rounded-2xl shadow-2xl flex flex-col justify-center items-center">
             <img
               src={process.env.PUBLIC_URL + "/merch/2021_Hoodie_Main.png"}
