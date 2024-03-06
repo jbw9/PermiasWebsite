16cd5639a39fb279f260ad2ea1fc914b2754f480
Jonathan Bernard Widjajakusuma <jbw7@illinois.edu>
2024-03-06 08:40:48 -0600

    changed bg color

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
     <Router>
       <div className="flex flex-col min-h-screen">
         <Header />
-        <main className="flex-grow">
+        <main className="flex-grow bg-groundBase">
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/team" element={<TeamPage />} />
