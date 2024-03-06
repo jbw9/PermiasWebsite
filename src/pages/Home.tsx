eac48fdbbb8d8f925c080b8b7210ba51d187c6b2
Jonathan Bernard Widjajakusuma <jbw7@illinois.edu>
2024-03-06 15:44:26 -0600

    dynamic upcomingevent component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -117,8 +117,25 @@ const HomePage: React.FC = () => {
           </div>
           <div className="mt-[75px] text-center">
             <span className="text-4xl">Upcoming Events</span>
-            <div className="flex justify-center">
-              <UpcomingEvents />
+            <div className="flex justify-center space-x-[250px] mt-[20px]">
+              <UpcomingEvents
+                eventCoverImage="gradnight"
+                eventName="Grad Night"
+                eventDate={10}
+                eventMonth="May"
+                eventLocation="Illini Union"
+                eventTime="7:00 - 9:00 pm"
+                eventrsvpLink="https://www.google.com/forms/about/"
+              />
+              <UpcomingEvents
+                eventCoverImage="satefundraising"
+                eventName="Sate Fundraising"
+                eventDate={8}
+                eventMonth="April"
+                eventLocation="Illini Union"
+                eventTime="1:00 - 3:00 pm"
+                eventrsvpLink="https://www.google.com/forms/about/"
+              />
             </div>
           </div>
         </div>
