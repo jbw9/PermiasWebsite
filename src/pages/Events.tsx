928aa8e415bb786c4d3690eb64a535aaf7f2466c
Evan243 <evanmhuang@gmail.com>
2024-03-07 14:30:13 -0600

    progress on events page

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import UpcomingEvents from "../components/events/upcomingEvents";
 
 const EventPage: React.FC = () => {
   return (
@@ -10,11 +11,33 @@ const EventPage: React.FC = () => {
           className="object-cover w-full h-full"
         />
       </div>
-      <div className="flex flex-col">
-        <span className="text-5xl font-semibold">Upcoming Events</span>
-        <div className="w-full h-[400px] bg-slate-500"></div>
-        <span className="text-5xl font-semibold mt-[40px]">Past Events</span>
-        <div className="w-full h-[400px] bg-slate-500"></div>
+      <div className="text-center mt-[50px] bg-white">
+        <span className="text-5xl font-semibold">Our Upcoming Events</span>
+        <div className="flex justify-center space-x-[250px] mt-[20px]">
+          <UpcomingEvents
+            eventCoverImage="gradnight"
+            eventName="Grad Night"
+            eventDate={10}
+            eventMonth="May"
+            eventLocation="Illini Union"
+            eventTime="7:00 - 9:00 pm"
+            eventrsvpLink="https://www.google.com/forms/about/"
+          />
+          <UpcomingEvents
+            eventCoverImage="satefundraising"
+            eventName="Sate Fundraising"
+            eventDate={8}
+            eventMonth="April"
+            eventLocation="Illini Union"
+            eventTime="1:00 - 3:00 pm"
+            eventrsvpLink="https://www.google.com/forms/about/"
+          />
+        </div>
+        <div>
+
+        </div>
+        <span className="text-5xl font-semibold mt-[80px]">Our Past Events</span>
+        <div className="w-full h-[400px] bg-white"></div>
       </div>
     </div>
   );
