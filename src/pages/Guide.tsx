b4f714412b75949dd72751d9695b1143e4385c0c
Evan243 <evanmhuang@gmail.com>
2024-03-10 20:15:14 -0500

    guide page progress

--- src/pages/Guide.tsx
+++ src/pages/Guide.tsx
@@ -3,7 +3,31 @@ import React from "react";
 const GuidePage: React.FC = () => {
   return (
     <div>
-      <div>guide page</div>
+      <div className="flex flex-col mt-[25px]">
+        <span className="text-5xl text-center">Getting To UIUC</span>
+        <div className="flex flex-row justify-center space-x-[250px]">
+          <div>
+            <span className="text-2xl ml-[-100px]">Peoria Charter</span>
+          </div>
+          <div>
+            <span className="text-2xl ml-[50px]">Renting A Car</span>
+          </div>
+          <div>
+            <span className="text-2xl ml-[100px]">Uber</span>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-cols-2 space-x-[250px] justify-center mt-[25px] ">
+        <div className="flex flex-col">
+          <span className="text-5xl">Urbana-Champaign</span>
+          <span className="text-2xl"> Getting Around Campus</span>
+          <span className=""></span>
+        </div>
+        <div>
+          <span className="text-5xl">Campustown</span>
+        </div>
+      </div>
+
     </div>
   );
 };
