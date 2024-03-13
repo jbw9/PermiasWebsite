b948eb4bd331465b3d9b1306a657fb7d9bf2d5ee
Jonathan Bernard Widjajakusuma <jbw7@illinois.edu>
2024-03-13 03:20:33 -0400

    fixed scrolling position issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from "react";
-
-// Components
+import React, { useEffect } from "react";
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
-// Pages
 import HomePage from "./pages/Home";
 import TeamPage from "./pages/Team";
 import AboutUsPage from "./pages/AboutUs";
@@ -14,12 +16,23 @@ import GuidePage from "./pages/Guide";
 import MerchPage from "./pages/Merch";
 import NotFound from "./pages/NotFound";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
         <Header />
-        <main className="flex-grow bg-white mt-16">
+        <ScrollToTop />
+        <main className="flex-grow mt-16 bg-white">
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/team" element={<TeamPage />} />
