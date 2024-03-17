73a627e020867c4cfc4a8e4c8a2c81721632e0df
hindra2 <harold.indra@gmail.com>
2024-03-17 15:24:12 -0500

    minor changes

--- src/components/guide/card.tsx
+++ src/components/guide/card.tsx
@@ -9,37 +9,36 @@ interface GuideCardProps {
 
 const GuideCard: React.FC<GuideCardProps> = ({ imagePath, title, pros, cons }) => {
   return (
-    <div>
-        <div className="text-center shadow-xl rounded-xl">
-            <img
-              className="shadow-xl rounded-t-xl"
-              src={process.env.PUBLIC_URL + imagePath}
-              alt={imagePath}
-              style={{ width: "325px", height: "210px" }}
-            />
-            <span className="text-2xl text-center font-extralight">{ title }</span>
-            <div className="flex flex-cols-3 space-x-[15px]">
-              <div className="text-sxl">
-                <span className="underline">Pros</span>
-                <ul className="list-disc ml-[3px] pl-5">
-                  {pros.map((pro, index) => (
-                    <li key={index} className="text-sl">{pro}</li>
-                  ))}
-                </ul>
-              </div>
-              <div className="h-[full] w-[2px] bg-black"></div>
-              <div className="text-ssxl">
-                <span className="underline">Cons</span>
-                <ul className="list-disc ml-[3px] pl-5">
-                  {cons.map((con, index) => (
-                    <li key={index} className="text-sl">{con}</li>
-                  ))}
-                </ul>
-              </div>
-            </div>
+    <div className="text-center shadow-xl rounded-xl">
+      <img
+        className="shadow-xl rounded-t-xl"
+        src={process.env.PUBLIC_URL + imagePath}
+        alt={imagePath}
+        style={{ width: "325px", height: "210px" }}
+      />
+      <span className="text-2xl text-center font-extralight">{ title }</span>
+      <div className="flex flex-cols-3 space-x-[15px]">
+        <div className="text-sxl">
+          <span className="underline">Pros</span>
+          <ul className="list-disc ml-[3px] pl-5">
+            {pros.map((pro, index) => (
+              <li key={index} className="text-sl">{pro}</li>
+            ))}
+          </ul>
         </div>
+        <div className="h-[full] w-[2px] bg-black"></div>
+          <div className="text-ssxl">
+            <span className="underline">Cons</span>
+            <ul className="list-disc ml-[3px] pl-5">
+              {cons.map((con, index) => (
+                <li key={index} className="text-sl">{con}</li>
+              ))}
+            </ul>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default GuideCard;
+
