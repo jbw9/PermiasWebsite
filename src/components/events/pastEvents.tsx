b67fbf72299f0580d296f2e1ae664fada722a7f4
Jonathan Bernard Widjajakusuma <jbw7@illinois.edu>
2024-03-22 02:10:47 -0500

    modified carousel frequency and direction

--- src/components/events/pastEvents.tsx
+++ src/components/events/pastEvents.tsx
@@ -1,15 +1,16 @@
+import React from "react";
 import Carousel from "../events/carousel";
 import { pastEvents, PastEvent } from "../../data/past_events";
 
 const PastEvents = () => {
   return (
     <div>
-      {pastEvents.map((event: PastEvent) => (
-        <div className="mb-[100px]">
+      {pastEvents.map((event: PastEvent, index: number) => (
+        <div className="mb-[100px]" key={event.name}>
           <div className="relative flex flex-col mr-[40px]">
             <div className="absolute w-6 h-6 rounded-full bg-red mt-[11px]"></div>
           </div>
-          <div key={event.name} className="w-full ml-[40px]">
+          <div className="w-full ml-[40px]">
             <div className="mb-[5px] mt-[5px] items-start">
               <div className="flex text-3xl whitespace-nowrap">
                 {event.name}
@@ -17,7 +18,10 @@ const PastEvents = () => {
               <div className="flex text-xl whitespace-nowrap">{event.date}</div>
             </div>
             <div className="overflow-hidden">
-              <Carousel images={event.images} />
+              <Carousel
+                images={event.images}
+                direction={index % 2 === 0 ? "right" : "left"}
+              />
             </div>
           </div>
         </div>
