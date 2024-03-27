1a5efcf0689ece8bf1246667da8f8e9589fbbac3
Jonathan Bernard Widjajakusuma <jbw7@illinois.edu>
2024-03-27 14:59:24 -0500

    disabled horizontal scrolling and adjusted alert layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -32,57 +32,62 @@ const HomePage: React.FC = () => {
   };
 
   return (
-    <div className="mb-[100px]">
-      {showAlert && (
-        <div className="fixed z-50 p-4 text-center text-white bg-red ml-[50px] mt-[100px] rounded-2xl w-[200px]">
-          <button
-            onClick={handleCloseAlert}
-            className="float-right text-xl font-bold"
-          >
-            ✕
-          </button>
-          <p>
-            Sorry, this website is not optimized for mobile viewing yet. Please
-            toggle the desktop site mode or open it on your laptop/desktop.
-          </p>
-        </div>
-      )}
+    <div>
+      <div className="mb-[100px] w-full overflow-x-hidden">
+        {showAlert && (
+          <div className="fixed inset-0 z-50 flex items-center justify-center">
+            <div className="bg-red text-white p-4 rounded-2xl w-[300px] text-center">
+              <button
+                onClick={handleCloseAlert}
+                className="absolute text-xl font-bold top-2 right-2"
+              >
+                ✕
+              </button>
+              <p>
+                Sorry, this website is not optimized for mobile viewing yet.
+                Please toggle the desktop site mode or open it on your
+                laptop/desktop.
+              </p>
+            </div>
+          </div>
+        )}
 
-      <div>
-        <BackgroundVideo
-          source={process.env.PUBLIC_URL + "/Home/backgroundvid.mp4"}
-          children={<WebsiteTitle />}
-        />
-      </div>
-      <div className="flex items-center justify-center w-full">
-        <div className="w-full mt-[120px] flex flex-col items-center px-[150px]">
-          <AboutCommunity />
-          <div className="mt-[100px] text-center w-full">
-            <div>
-              <span className="text-5xl font-semibold text-footer">
-                Upcoming{" "}
-              </span>
-              <span className="text-5xl font-bold text-red">Events</span>
+        <div>
+          <BackgroundVideo
+            source={process.env.PUBLIC_URL + "/Home/backgroundvid.mp4"}
+            children={<WebsiteTitle />}
+          />
+        </div>
+        <div className="flex items-center justify-center w-full">
+          <div className="w-full mt-[120px] flex flex-col items-center px-[150px]">
+            <AboutCommunity />
+            <div className="mt-[100px] text-center w-full">
+              <div>
+                <span className="text-5xl font-semibold text-footer">
+                  Upcoming{" "}
+                </span>
+                <span className="text-5xl font-bold text-red">Events</span>
+              </div>
+              <div>
+                <UpcomingEventsList />
+              </div>
+              <div className="mt-[20px] flex justify-center">
+                <Link to="/events" className="inline-block mb-4">
+                  <div className="bg-red rounded-2xl w-[200px] h-[50px] flex justify-center items-center transition-transform duration-300 ease-in-out transform hover:-translate-y-2">
+                    <span className="text-white">View Past Events</span>
+                  </div>
+                </Link>
+              </div>
             </div>
-            <div>
-              <UpcomingEventsList />
+            <div className="mt-[100px]">
+              <OurPurpose />
             </div>
-            <div className="mt-[20px] flex justify-center">
-              <Link to="/events" className="inline-block mb-4">
-                <div className="bg-red rounded-2xl w-[200px] h-[50px] flex justify-center items-center transition-transform duration-300 ease-in-out transform hover:-translate-y-2">
-                  <span className="text-white">View Past Events</span>
-                </div>
-              </Link>
+            <div className="mt-[140px] mx-[150px]">
+              <FreshmanSpotlight />
             </div>
-          </div>
-          <div className="mt-[100px]">
-            <OurPurpose />
-          </div>
-          <div className="mt-[140px] mx-[150px]">
-            <FreshmanSpotlight />
-          </div>
-          <div className="mt-[120px]">
-            <GetInvolved />
+            <div className="mt-[120px]">
+              <GetInvolved />
+            </div>
           </div>
         </div>
       </div>
