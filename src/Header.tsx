4673f910e5851dade024f332e714419f42a3ed38
wrjodjana <141975732+wrjodjana@users.noreply.github.com>
2024-03-28 04:59:03 -0500

    changed about us to about for mobile devices (header)

--- src/Header.tsx
+++ src/Header.tsx
@@ -10,77 +10,29 @@ const Header: React.FC = () => {
   return (
     <div className="fixed top-0 left-0 z-10 flex items-center justify-between w-full p-2 bg-white shadow-xl">
       <Link to="/">
-        <img
-          src={process.env.PUBLIC_URL + "/mainLogo.png"}
-          alt="PERMIAS LOGO"
-          width={250}
-        />
+        <img src={process.env.PUBLIC_URL + "/mainLogo.png"} alt="PERMIAS LOGO" width={250} />
       </Link>
       <nav className="flex items-center ml-4">
-        <Link
-          to="/"
-          className={`mx-2 text-sxl text-footer relative inline-block group ${isActive(
-            "/"
-          )}`}
-        >
+        <Link to="/" className={`mx-2 text-sxl text-footer relative inline-block group ${isActive("/")}`}>
           Home
-          <span
-            className={`absolute bg-red bottom-0 left-0 right-0 h-0.5 bg-current scale-x-0 group-hover:scale-x-100 transform transition-transform ease-out duration-150 ${
-              isActive("/") ? "scale-x-100" : ""
-            }`}
-          ></span>
+          <span className={`absolute bg-red bottom-0 left-0 right-0 h-0.5 bg-current scale-x-0 group-hover:scale-x-100 transform transition-transform ease-out duration-150 ${isActive("/") ? "scale-x-100" : ""}`}></span>
         </Link>
-        <Link
-          to="/about-us"
-          className={`mx-2 text-sxl text-footer relative inline-block group ${isActive(
-            "/about-us"
-          )}`}
-        >
-          About Us
-          <span
-            className={`absolute bg-red bottom-0 left-0 right-0 h-0.5 bg-current scale-x-0 group-hover:scale-x-100 transform transition-transform ease-out duration-150 ${
-              isActive("/about-us") ? "scale-x-100" : ""
-            }`}
-          ></span>
+        <Link to="/about-us" className={`mx-2 text-sxl text-footer relative inline-block group ${isActive("/about-us")}`}>
+          <span className="hidden sm:inline">About Us</span>
+          <span className="inline sm:hidden">About</span>
+          <span className={`absolute bg-red bottom-0 left-0 right-0 h-0.5 bg-current scale-x-0 group-hover:scale-x-100 transform transition-transform ease-out duration-150 ${isActive("/about-us") ? "scale-x-100" : ""}`}></span>
         </Link>
-        <Link
-          to="/team"
-          className={`mx-2 text-sxl text-footer relative inline-block group ${isActive(
-            "/team"
-          )}`}
-        >
+        <Link to="/team" className={`mx-2 text-sxl text-footer relative inline-block group ${isActive("/team")}`}>
           Team
-          <span
-            className={`absolute bg-red bottom-0 left-0 right-0 h-0.5 bg-current scale-x-0 group-hover:scale-x-100 transform transition-transform ease-out duration-150 ${
-              isActive("/team") ? "scale-x-100" : ""
-            }`}
-          ></span>
+          <span className={`absolute bg-red bottom-0 left-0 right-0 h-0.5 bg-current scale-x-0 group-hover:scale-x-100 transform transition-transform ease-out duration-150 ${isActive("/team") ? "scale-x-100" : ""}`}></span>
         </Link>
-        <Link
-          to="/events"
-          className={`mx-2 text-sxl text-footer relative inline-block group ${isActive(
-            "/events"
-          )}`}
-        >
+        <Link to="/events" className={`mx-2 text-sxl text-footer relative inline-block group ${isActive("/events")}`}>
           Events
-          <span
-            className={`absolute bg-red bottom-0 left-0 right-0 h-0.5 bg-current scale-x-0 group-hover:scale-x-100 transform transition-transform ease-out duration-150 ${
-              isActive("/events") ? "scale-x-100" : ""
-            }`}
-          ></span>
+          <span className={`absolute bg-red bottom-0 left-0 right-0 h-0.5 bg-current scale-x-0 group-hover:scale-x-100 transform transition-transform ease-out duration-150 ${isActive("/events") ? "scale-x-100" : ""}`}></span>
         </Link>
-        <Link
-          to="/merch"
-          className={`mx-2 text-sxl text-footer relative mr-[50px] inline-block group ${isActive(
-            "/merch"
-          )}`}
-        >
+        <Link to="/merch" className={`mx-2 text-sxl text-footer relative mr-[50px] inline-block group ${isActive("/merch")}`}>
           Merch
-          <span
-            className={`absolute bg-red bottom-0 left-0 right-0 h-0.5 bg-current scale-x-0 group-hover:scale-x-100 transform transition-transform ease-out duration-150 ${
-              isActive("/merch") ? "scale-x-100" : ""
-            }`}
-          ></span>
+          <span className={`absolute bg-red bottom-0 left-0 right-0 h-0.5 bg-current scale-x-0 group-hover:scale-x-100 transform transition-transform ease-out duration-150 ${isActive("/merch") ? "scale-x-100" : ""}`}></span>
         </Link>
       </nav>
     </div>
