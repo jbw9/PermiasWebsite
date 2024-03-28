58e0f138cff94cf14f1da36553070772198d1817
wrjodjana <141975732+wrjodjana@users.noreply.github.com>
2024-03-28 04:54:44 -0500

    added mobile responsiveness to About Us Page

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -3,85 +3,50 @@ import React from "react";
 const AboutUsPage: React.FC = () => {
   return (
     <div className="">
-      <div className="w-full h-[600px] flex justify-center items-end mb-[100px]">
-        <img
-          src={process.env.PUBLIC_URL + "/AboutUs/background.png"}
-          alt="Background"
-          className="object-cover w-full h-full"
-        />
+      <div className="w-full h-auto flex justify-center items-end mb-8">
+        <img src={process.env.PUBLIC_URL + "/AboutUs/background.png"} alt="Background" className="object-cover w-full md:h-[600px]" />
       </div>
       <div className="container px-4 mx-auto">
-        <div className="flex flex-col md:h-full md:flex md:flex-row md:items-center">
-          <div className="flex flex-col justify-center md:w-1/2">
-            <h1 className="mb-4 text-5xl font-bold text-left text-red">
-              About Us
-            </h1>
-            <span className="text-xl text-left">
-              PERMIAS UIUC, or commonly known as the Indonesian Students Club
-              (ISC), is an Indonesian community in the heart of Urbana -
-              Champaign. We strive to unite the Indonesian community within the
-              University of Illinois at Urbana-Champaign while also promoting
-              our culture to the greater community in Illinois.
+        <div className="flex flex-col md:flex-row md:items-center md:justify-between">
+          <div className="flex flex-col justify-center md:w-1/2 mb-8 md:mb-0 md:pr-8">
+            <h1 className="mb-4 text-5xl font-bold text-center md:text-left text-red">About Us</h1>
+            <span className="text-xl text-center md:text-left">
+              PERMIAS UIUC, or commonly known as the Indonesian Students Club (ISC), is an Indonesian community in the heart of Urbana - Champaign. We strive to unite the Indonesian community within the University of Illinois at
+              Urbana-Champaign while also promoting our culture to the greater community in Illinois.
             </span>
           </div>
-          <div className="flex justify-center w-full md:w-1/2 md:justify-end">
-            <img
-              className="shadow-2xl rounded-xl"
-              src={process.env.PUBLIC_URL + "/AboutUs/image1.png"}
-              alt="PERMIAS LOGO"
-              height={400}
-              width={550}
-            />
+          <div className="flex justify-center md:w-1/2">
+            <img className="shadow-2xl rounded-xl w-full md:w-auto md:h-[400px]" src={process.env.PUBLIC_URL + "/AboutUs/image1.png"} alt="PERMIAS LOGO" />
           </div>
         </div>
       </div>
 
       <div className="container px-4 pt-32 mx-auto">
-        <div className="flex flex-col-reverse md:h-full md:flex md:flex-row md:items-center">
-          <div className="md:w-1/2">
-            <img
-              className="shadow-2xl rounded-xl"
-              src={process.env.PUBLIC_URL + "/AboutUs/image2.png"}
-              alt="PERMIAS LOGO"
-              height={400}
-              width={550}
-            />
+        <div className="flex flex-col-reverse md:flex-row md:items-center md:justify-between">
+          <div className="flex justify-center md:w-1/2 mb-16 md:mb-0 md:mr-16">
+            <img className="shadow-2xl rounded-xl w-full md:w-auto md:h-[400px]" src={process.env.PUBLIC_URL + "/AboutUs/image2.png"} alt="PERMIAS LOGO" />
           </div>
-          <div className="flex flex-col justify-center md:w-1/2">
-            <h1 className="mb-4 text-5xl font-bold text-right text-red">
-              Our Vision
-            </h1>
-            <span className="text-xl text-right">
-              At PERMIAS UIUC, our vision extends beyond merely creating a fun
-              and safe environment for the Indonesian community in
-              Urbana-Champaign. We aspire to be a beacon of Indonesian culture,
-              heritage, and values in Illinois, fostering a deeper understanding
-              and appreciation among the diverse tapestry of communities we live
-              in.
+          <div className="flex flex-col justify-center md:w-1/2 md:ml-16">
+            <h1 className="mb-8 text-5xl font-bold text-center md:text-right text-red">Our Vision</h1>
+            <span className="text-xl text-center md:text-right">
+              At PERMIAS UIUC, our vision extends beyond merely creating a fun and safe environment for the Indonesian community in Urbana-Champaign. We aspire to be a beacon of Indonesian culture, heritage, and values in Illinois,
+              fostering a deeper understanding and appreciation among the diverse tapestry of communities we live in.
             </span>
           </div>
         </div>
       </div>
 
       <div className="container px-4 py-32 mx-auto">
-        <div className="flex flex-col md:flex md:h-full md:flex-row md:items-center">
-          <div className="flex flex-col justify-center md:w-1/2">
-            <h1 className="mb-4 text-5xl font-bold text-red">Our Mission</h1>
-            <span className="text-xl">
-              Our mission is to promote Indonesian culture to the
-              Urbana-Champaign community, provide networking opportunities for
-              Indonesian students, and connect PERMIAS UIUC members with the
-              wider Indonesian community in the Midwest area.
+        <div className="flex flex-col md:flex-row md:items-center md:justify-between">
+          <div className="flex flex-col justify-center md:w-1/2 mb-16 md:mb-0 md:pr-8">
+            <h1 className="mb-8 text-5xl font-bold text-center md:text-left text-red">Our Mission</h1>
+            <span className="text-xl text-center md:text-left">
+              Our mission is to promote Indonesian culture to the Urbana-Champaign community, provide networking opportunities for Indonesian students, and connect PERMIAS UIUC members with the wider Indonesian community in the Midwest
+              area.
             </span>
           </div>
-          <div className="flex justify-center md:w-1/2 md:justify-end">
-            <img
-              className="shadow-2xl rounded-xl"
-              src={process.env.PUBLIC_URL + "/AboutUs/image3.png"}
-              alt="PERMIAS LOGO"
-              height={400}
-              width={550}
-            />
+          <div className="flex justify-center md:w-1/2">
+            <img className="shadow-2xl rounded-xl w-full md:w-auto md:h-[400px]" src={process.env.PUBLIC_URL + "/AboutUs/image3.png"} alt="PERMIAS LOGO" />
           </div>
         </div>
       </div>
