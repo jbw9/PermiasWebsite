179ae3232c827735bcb9b3a1b18202f55b3850e6
Jonathan Bernard Widjajakusuma <jbw7@illinois.edu>
2024-10-08 11:35:25 -0500

    updated upcoming events

--- src/components/events/upcomingEventsList.tsx
+++ src/components/events/upcomingEventsList.tsx
@@ -4,20 +4,28 @@ import UpcomingEvents from "../events/upcomingEvents";
 const UpcomingEventsList = () => {
   return (
     <div>
-      {" "}
       <div className="flex flex-col items-center space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-8 md:space-x-12 lg:space-x-[150px] mt-[20px]">
-        {" "}
         <div className="w-full sm:w-auto">
-          {" "}
           <UpcomingEvents
-            eventCoverImage="meetmingle"
-            eventName="Mingle & Connect"
-            eventDate={31}
-            eventMonth="Aug"
-            eventLocation="Scott Park"
-            eventTime="10.30 - 2pm"
-          />{" "}
-        </div>{" "}
+            eventCoverImage="pietheofficer2024"
+            eventName="Pie The Officer"
+            eventDate={12}
+            eventMonth="Oct"
+            eventLocation="Main Quad"
+            eventTime="12.00 - 2.00pm"
+            eventrsvpLink="https://forms.gle/VFVh5ZMfseg8EKsm8"
+          />
+        </div>
+        <div className="w-full sm:w-auto">
+          <UpcomingEvents
+            eventCoverImage="pandji"
+            eventName="Friday Funnies - Pandji"
+            eventDate={18}
+            eventMonth="Oct"
+            eventLocation="Courtyard Cafe, Illini Union"
+            eventTime="7.00pm"
+          />
+        </div>
       </div>{" "}
     </div>
   );
